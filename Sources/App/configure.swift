fa841018c9dfc50556afc4e33d0258e0c66529d9
ElCruzo <ayomideadekoya266@gmail.com>
2023-10-22 07:40:56 -0400

    Modified the codebase

--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -5,23 +5,32 @@ import Leaf
 import Vapor
 
 // configures your application
-public func configure(_ app: Application) async throws {
+public func configure(_ app: Application) throws {
     // uncomment to serve files from /Public folder
     // app.middleware.use(FileMiddleware(publicDirectory: app.directory.publicDirectory))
 
-    app.databases.use(DatabaseConfigurationFactory.mysql(
-        hostname: Environment.get("DATABASE_HOST") ?? "localhost",
-        port: Environment.get("DATABASE_PORT").flatMap(Int.init(_:)) ?? MySQLConfiguration.ianaPortNumber,
-        username: Environment.get("DATABASE_USERNAME") ?? "vapor_username",
-        password: Environment.get("DATABASE_PASSWORD") ?? "vapor_password",
-        database: Environment.get("DATABASE_NAME") ?? "vapor_database"
-    ), as: .mysql)
+    let hostname = "localhost"
+    let port = 3306
+    let username = "your_username"
+    let password = "your_password"
+    let database = "your_database"
+
+    // Configure the database
+    app.databases.use(
+        .mysql(
+            hostname: hostname,
+            port: port,
+            username: username,
+            password: password,
+            database: database,
+            tlsConfiguration: .makeClientConfiguration(certificateVerification: .none),
+        ),
+    as: .mysql
+    )
 
     app.migrations.add(CreateTodo())
 
     app.views.use(.leaf)
-
-    
 
     // register routes
     try routes(app)
